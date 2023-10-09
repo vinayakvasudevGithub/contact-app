735298702474c4079f3c8754c2645f588a7ae025
vinayak <vinayakvasudev9876@gmail.com>
2023-10-09 15:11:15 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         click here to Learn React
         </a>
       </header>
     </div>
