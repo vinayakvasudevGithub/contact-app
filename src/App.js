00ee9965e559cbbe2eda2d803506b005dbd90758
vinayak <vinayakvasudev9876@gmail.com>
2023-10-09 15:41:59 -0700

    third change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         click here to Learn React
+         click here to Learn React and how to learn react
         </a>
       </header>
     </div>
